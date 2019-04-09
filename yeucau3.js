4d2a6284d6a84b6359e7d98b21f4cda08abf37de
nguyenvantu96 <36929721+nguyenvantu96@users.noreply.github.com>
2019-04-10 01:26:18 +0700

    cas
    cas

--- yeucau3.js
+++ yeucau3.js
@@ -1,1 +1,51 @@
 
+
+const fs = require('fs');
+var listFile = [];
+var listFolder = [];
+var listlinhtin = [];
+
+
+var deleteTxt = 'vantu';
+const { exec } = require('child_process');
+var q = 0;
+
+function filter(testFolder) {
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+            if (file.endsWith('.rar') || file.endsWith('.zip')) {
+                listFile.push(testFolder+file)
+            } else {
+                var stats = fs.statSync(testFolder + file);
+                if (stats.isDirectory()) {
+                    q++;
+                   
+                    filter(testFolder + file + '/');
+                } 
+                if(stats.isFile()){
+                    listlinhtin.push(testFolder+ file)
+                }
+            }
+        });
+        q--;
+        if(q==0) {
+         deleteFile();
+        }
+    });
+}
+
+
+function deleteFile(){
+    listlinhtin.forEach(element=>{
+        if(element.search(deleteTxt) >= 0 && deleteTxt != ''){
+            exec(`rm -rf ${element}`)
+        }
+    })
+}
+
+function main (){
+    q++;
+    filter('./');
+   
+}
+main();
