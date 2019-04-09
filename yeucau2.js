b92b7e81f331e8bbe10b53b19a37e2aeed9ee2dd
nguyenvantu96 <36929721+nguyenvantu96@users.noreply.github.com>
2019-04-10 01:21:02 +0700

    ca
    c

--- yeucau2.js
+++ yeucau2.js
@@ -1,1 +1,63 @@
 
+
+
+const fs = require('fs');
+var listFile = [];
+var listFolder = [];
+var listlinhtin = [];
+var listRename=[];
+
+var deleteTxt = '';
+const { exec } = require('child_process');
+var q = 0;
+
+function filter(testFolder) {
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+            if (file.endsWith('.rar') || file.endsWith('.zip')) {
+                if (q != 1){
+                    listFile.push(testFolder+file)
+                }
+            } else {
+                var stats = fs.statSync(testFolder + file);
+                if (stats.isDirectory()) {
+                    q++;
+                   
+                    filter(testFolder + file + '/');
+                } 
+                if(stats.isFile()){
+                    listlinhtin.push(testFolder+ file)
+                }
+            }
+        });
+        q--;
+        if(q==0) {
+        
+        ex();
+    
+        }
+    });
+}
+
+
+
+function ex() {
+  listFile.forEach(filename => {
+    const path = filename.substr(0, filename.lastIndexOf('/') + 1);
+    if (filename.endsWith('.zip')) {
+      
+      exec(`unzip '${filename}' -d ${path} && rm -rf '${filename}'`)
+    } 
+    if (filename.endsWith('.rar')) {
+             exec(`unrar e '${filename}' ${path} && rm -rf '${filename}'`)
+    }
+
+  });
+ 
+}
+function main (){
+    q++;
+    filter('./');
+   
+}
+main();
